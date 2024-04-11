c1bcc46e402fe14dec2aedd2ba9b0f39c2b5c57d
Sebastian Vega <svegalopez@gmail.com>
2024-04-11 14:42:44 -0400

    updates to vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      // Proxying API requests from / to a local server on port 5078
+      "/": {
+        target: "http://localhost:5078",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+      // You can add more proxy rules here
+    },
+  },
   plugins: [react()],
-})
+});
